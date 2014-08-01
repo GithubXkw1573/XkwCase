a830488845acf049597cd1fd2e4379a5fe4cadf9
许开伟 <xukaiwei@xukaiweideMacBook-Air.local>
2014-08-01 20:45:35 +0800

    Initial Commit

---
+++ XkwCase/XCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XCAppDelegate.h
+//  XkwCase
+//
+//  Created by 许开伟 on 14-8-1.
+//  Copyright (c) 2014年 ___Xukaiwei___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ XkwCase/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  XkwCase
+//
+//  Created by 许开伟 on 14-8-1.
+//  Copyright (c) 2014年 ___Xukaiwei___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "XCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([XCAppDelegate class]));
+    }
+}
